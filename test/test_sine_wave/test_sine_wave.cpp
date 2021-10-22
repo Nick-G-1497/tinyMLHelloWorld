6c86d927f829b6af92943f7df55b43f00ba645aa
Nick-G-1497 <NickGoralka@gmail.com>
2021-10-22 15:40:23 -0600

    simplify tests

--- test/test_sine_wave/test_sine_wave.cpp
+++ test/test_sine_wave/test_sine_wave.cpp
@@ -4,28 +4,27 @@
 #ifdef UNIT_TEST
 
 
-// #include <Arduino.h>
+void test_function_foo(void) {
+    TEST_ASSERT_EQUAL(1, 1);
+}
+
+void process() {
+    UNITY_BEGIN();  
+    
+    RUN_TEST(test_function_foo);
+
+    UNITY_END();
+}
+
+
 void setup() {
     
     process();
 }
 
 void loop() {
-    // digitalWrite(13, HIGH);
-    // delay(100);
-    // digitalWrite(13, LOW);
-    // delay(500);
+
 }
 
-// #else
-
-// int main(int argc, char **argv) {
-    
-    
-//     process();
-//     return 0;
-// }
-
-// #endif
 
 #endif
