b04b529ee920b19c7af155f3091558e68aa67d90
Nick-G-1497 <NickGoralka@gmail.com>
2021-10-22 15:35:01 -0600

    Test

---
+++ test/test_sine_wave/test_sine_wave.cpp
@@ -0,0 +1,31 @@
+#include "unity.h"
+
+
+#ifdef UNIT_TEST
+
+
+// #include <Arduino.h>
+void setup() {
+    
+    process();
+}
+
+void loop() {
+    // digitalWrite(13, HIGH);
+    // delay(100);
+    // digitalWrite(13, LOW);
+    // delay(500);
+}
+
+// #else
+
+// int main(int argc, char **argv) {
+    
+    
+//     process();
+//     return 0;
+// }
+
+// #endif
+
+#endif
